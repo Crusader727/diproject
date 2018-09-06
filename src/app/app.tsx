31761001a87eac2f550138a095a66f9665fb6832
Crusader727 <rubenhov97@gmail.com>
2018-09-06 17:10:42 +0300

    start

---
+++ src/app/app.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Input from 'components/input/input';
+
+const App = () => <div className="app"><Input /></div>;
+
+export default App;
---
+++ src/components/input/input.tsx
@@ -0,0 +1,6 @@
+import './input.scss';
+import * as React from "react";
+
+const Input = () => <div className="input">Hello !</div>;
+
+export default Input;
