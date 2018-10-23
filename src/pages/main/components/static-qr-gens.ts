026b47803c9b6d3c96b8601cbf8693061262b8a6
Crusader727 <rubenhov97@gmail.com>
2018-10-23 12:24:26 +0300

    qr gen fix small

--- src/pages/main/components/static-qr-gens.ts
+++ src/pages/main/components/static-qr-gens.ts
@@ -6,7 +6,7 @@ const StaticQrGens: Record<string, (values: string[]) => string> = {
         return `tel:${values[0]}`;
     },
     'sms': (values: string[]): string => {
-        return encodeURI(`sms:${values[0]}?;body=${values[1]}`);
+        return encodeURI(`sms:${values[0]};body=${values[1]}`);
     },
     'event': (values: string[]): string => {
         const date1 = values[2].replace(/[\:\-]/g,'') + '00';
